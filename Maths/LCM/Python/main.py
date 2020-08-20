41edef5a22cdd8618e9f8346d63d891c490802c2
gsteja2307 <30834202+gsteja2307@users.noreply.github.com>
2020-08-20 22:53:16 +0530

    LCM solution

--- Maths/LCM/Python/main.py
+++ Maths/LCM/Python/main.py
@@ -1,1 +1,16 @@
+#1
 
+#x * y = gcd(x,y) * lcm(x,y)
+# lcm(x,y) = (x * y) / gcd(x,y)
+
+def gcd(a,b):
+  if b==0:
+    return a
+  else:
+    return gcd(b,int(a%b))
+
+def lcm(x,y):
+  return int((x*y)/gcd(x,y))
+
+print(lcm(3,4))
+  
