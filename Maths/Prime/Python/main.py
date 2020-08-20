00c0276f3820f02f06204d315fd97aaac2868fc1
gsteja2307 <30834202+gsteja2307@users.noreply.github.com>
2020-08-20 23:36:40 +0530

    Check Prime Solution

--- Maths/Prime/Python/main.py
+++ Maths/Prime/Python/main.py
@@ -19,3 +19,30 @@ print(prime(101))
 print(prime(97))
 print(prime(49))
         
+#2
+#more efficient solution
+
+from math import sqrt
+
+def prime(x):
+  print(x)
+  if x == 1:
+    return False
+  if x == 2 or x==3:
+    return True
+  if x%2 ==0 or x%3==0:
+    return False
+  else:
+    m = int(sqrt(x)) + 1
+    res = True
+    for i in range (5,m,6):
+      if x%i == 0 or x%(i+2) == 0:
+        res = False
+    return res
+
+print(prime(21))
+print(prime(101))
+print(prime(97))
+print(prime(49))
+print(prime(1031))
+        
