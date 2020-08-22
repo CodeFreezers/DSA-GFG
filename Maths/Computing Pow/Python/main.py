a9cdbf0303e02c79654130559764c7e3564fb47d
gsteja2307 <30834202+gsteja2307@users.noreply.github.com>
2020-08-22 21:56:56 +0530

    Computing Power solution

--- Maths/Computing Pow/Python/main.py
+++ Maths/Computing Pow/Python/main.py
@@ -1,1 +1,20 @@
+#1
+def power (x, n):
+  res = 1
+  for i in range(n):
+    res = res * x
+  return res
 
+#2
+def power(x, n):
+  if n == 0:
+    return 1
+  temp = power (x, int(n/2))
+  temp = temp * temp
+  if n % 2 == 0:
+    return temp
+  else:
+    return temp * x
+
+
+print(power(5, 3))
