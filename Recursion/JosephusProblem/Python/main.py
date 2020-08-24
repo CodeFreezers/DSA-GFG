f0764c38ded90d42fc784cfe16eb60fe2f92a7d3
gsteja2307 <30834202+gsteja2307@users.noreply.github.com>
2020-08-25 00:04:22 +0530

    Recursive Solution for Josephus Problem

---
+++ Recursion/JosephusProblem/Python/main.py
@@ -0,0 +1,22 @@
+#1
+def josp(n, k):
+  a=[0]*n
+  for i in range(n):
+    a[i]=i
+  helperFunc(n, k, a, 0)
+  return a[0]
+
+def helperFunc(n, k, a, idx):
+  if idx >= len(a):
+    idx = idx - len(a) 
+  if n==1:
+    return a
+  temp = idx + k - 1
+  if temp >= len(a):
+    temp -= len(a)
+  a.pop(temp)
+  # print(a) un comment this line to see the solution as visualisation
+  helperFunc(n-1, k, a, temp)
+
+print(josp(7, 3))
+print(josp(5, 3))
