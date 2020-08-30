87503d3c8299a1089926daf53860464deaa44c42
gsteja2307 <30834202+gsteja2307@users.noreply.github.com>
2020-08-30 05:41:39 +0530

    Solution to Check if an array is sorted

---
+++ Arrays/CheckIfArrayIsSorted/Python/main.py
@@ -0,0 +1,16 @@
+#1
+def CheckIfArrayIsSorted(arr : list) -> bool:
+  if len(arr) == 0 :
+    return False
+  if len(arr) == 1 :
+    return True
+  for i in range(1,len(arr)):
+    if arr[i] < arr[i-1]:
+      return False
+  return True
+
+print(CheckIfArrayIsSorted([1,3,4,9,6,878]))
+
+print(CheckIfArrayIsSorted([1,3,4,6,8,9,3123,99]))
+
+print(CheckIfArrayIsSorted([1,3,4,6,8,9,99]))
